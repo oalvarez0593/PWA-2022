c2a13ac31adf8db2f290265562aa765d9b2e7a5b
Omar Alvarez <oalvareze11@gmail.com>
2022-03-22 17:14:58 -0600

    evento install del SW

--- 05-Modulo ciclo vida SW y listeners/sw.js
+++ 05-Modulo ciclo vida SW y listeners/sw.js
@@ -1,3 +1,10 @@
 
 // Ciclo de vida del SW
 
+self.addEventListener('install', event => {
+    /* CUALQUIER INSTALACIÓN DE SW, EL EVENTO INSTALL
+       SIEMPRE VA A INSTALAR EL NUEVO */
+    console.log('SW: Instalando SW');
+    console.log(event);
+
+});
