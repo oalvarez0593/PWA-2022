b88f6801292592c6bb26d41d94e67ad515cf1b98
Omar Alvarez <oalvareze11@gmail.com>
2022-03-22 16:58:48 -0600

    Completo el modulo 4 de los Service workers y eventos fetch

--- 04-Intro a los Service Worker/sw.js
+++ 04-Intro a los Service Worker/sw.js
@@ -1,1 +1,27 @@
-console.log('SW: Hola mundo 2');
+self.addEventListener('fetch', event => {
+    /* console.log(event); */
+
+    /* LA REPSUESTA DEL NUEVO EVENTO SERÁ LO QUE VENGA
+       AL HACER LA PETICIÓN */
+
+    // event.respondWith(fetch(event.request));
+
+
+    /* CONSULTO EN TODOS LOS REQUEST SI ALGUNO INCLUYE JPG
+       Y LOS IMPRIMO EN CONSOLA */
+
+    if (event.request.url.includes('.jpg')) {
+        console.log(event.request.url);
+        /* event.request.url(null); */
+
+        // let fotoRequest = fetch('img/main.jpg');
+        //  let fotoRequest = fetch(event.request.url);
+        // let fotoRequest = fetch(event.request);
+
+        /* event.respondWith(null); */
+        let foto = fetch('img/main-patas-arriba.jpg');
+        event.respondWith(foto);
+    } else {
+        event.respondWith(fetch(event.request));
+    }
+});
