d26d516fe204d6f02170fd7cb8b89e9d4a17b216
Omar Alvarez <oalvareze11@gmail.com>
2022-03-24 16:38:34 -0600

    Como utilizar el activate y el event.waitUntil()

--- 05-Modulo ciclo vida SW y listeners/sw.js
+++ 05-Modulo ciclo vida SW y listeners/sw.js
@@ -4,7 +4,32 @@
 self.addEventListener('install', event => {
     /* CUALQUIER INSTALACIÓN DE SW, EL EVENTO INSTALL
        SIEMPRE VA A INSTALAR EL NUEVO */
-    console.log('SW: Instalando SW');
+    console.log('SW: Instalando SW!!');
     console.log(event);
 
+
+    /* Instrucción para que el SW se active inmediatamente */
+    // --> self.skipWaiting();
+
+    const instalacion = new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            console.log('SW: Instalación terminada');
+            self.skipWaiting();
+            return resolve();
+        }, 2000);
+    });
+
+    event.waitUntil(instalacion);
+    
+
+});
+
+
+/* CUANDO EL SW TOMA EL CONTROL DE LA APP */
+
+self.addEventListener('activate', event => {
+    /* Aqui es un muy buen lugar para borrar caché viejo */
+
+    console.log('SW2: activo y listo para funcionar');
 });
