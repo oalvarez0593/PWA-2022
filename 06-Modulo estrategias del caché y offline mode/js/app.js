aa31f2e104420d1bc2ed6ddc8812c3deec2c04a9
Omar Alvarez <oalvareze11@gmail.com>
2022-09-26 18:39:15 -0600

    Pruebas de clases en modulo "Estrategias de cache"

--- 06-Modulo estrategias del caché y offline mode/js/app.js
+++ 06-Modulo estrategias del caché y offline mode/js/app.js
@@ -2,20 +2,27 @@
 
 // Detectar si podemos usar Service Workers
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js')
-        .then(reg => {
-            setTimeout(() => {
-                reg.sync.register('posteo-gatitos');
-                console.log('Se enviaron las fotos de los gatos al server.');
-            }, 3000);
-            
-            Notification.requestPermission().then(result => {
-                console.log(result);
-                reg.showNotification('Hola mundo');
-            })
-        })
+    navigator.serviceWorker.register('/sw.js');
 }
 
-/* fetch('https://reqres.in/api/users')
-    .then(resp => resp.text())
-    .then(console.log) */
+if (window.caches) {
+    caches.open('prueba-1');
+    //Pregunta si existe prueba-1
+    caches.has('prueba-1').then(existe => console.log(existe));
+
+    //Se borra del caché el prueba-1
+    caches.delete('prueba-1').then(borrado => console.log(borrado));
+
+    caches.open('cache-v1.1').then(cache => {
+        /* cache.add('/pages/offline.html'); */
+        cache.addAll([
+            '/pages/offline.html',
+            '/img/main.jpg',
+            '/css/style.css'
+        ]);
+
+        cache.match('/pages/offline.html').then(resp => {
+            resp.text().then(imprimir => console.log(imprimir));
+        })
+    })
+}
--- 06-Modulo estrategias del caché y offline mode/sw.js
+++ 06-Modulo estrategias del caché y offline mode/sw.js
@@ -1,60 +1,27 @@
-
-// Ciclo de vida del SW
-
 self.addEventListener('install', event => {
-    /* CUALQUIER INSTALACIÓN DE SW, EL EVENTO INSTALL
-       SIEMPRE VA A INSTALAR EL NUEVO */
-    console.log('SW: Instalando SW!!');
-    console.log(event);
 
-
-    /* Instrucción para que el SW se active inmediatamente */
-    self.skipWaiting();
-
-    const instalacion = new Promise((resolve, reject) => {
-
-        setTimeout(() => {
-            console.log('SW: Instalación terminada');
-            self.skipWaiting();
-            return resolve();
-        }, 1);
+    const cachePromise = caches.open('cache-1').then(cache => {
+        return cache.addAll([
+            '/',
+            '/index.html',
+            '/pages/offline.html',
+            '/css/style.css',
+            '/img/main.jpg',
+            'img/favicon.ico',
+            'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+            '/js/app.js'
+        ]);
     });
 
-    event.waitUntil(instalacion);
-
-
-});
-
-
-/* CUANDO EL SW TOMA EL CONTROL DE LA APP */
-
-self.addEventListener('activate', event => {
-    /* Aqui es un muy buen lugar para borrar caché viejo */
-
-    console.log('SW2: activo y listo para funcionar');
-});
-
-/* FETCH: manejo de peticiones HTTP */
+    event.waitUntil(cachePromise);
+})
 
 self.addEventListener('fetch', event => {
-    /* Aplicar las estrategias del caché */
-    /* console.log('SW3:', event.request.url);
 
-    if (event.request.url.includes('https://reqres.in/')) {
-        const resp = new Response(`{ ok: false, message: 'jajaja'}`);
-        event.respondWith(resp);
-    } */
+    //CACHE ONLY
+    /* Cuando la app cargará toda desde el caché, nunca va a requerir de internet */
+
+    event.respondWith(caches.match(event.request));
+
 
 });
-
-//SYNC: Es cuando recuperamos la conexión a internet.
-self.addEventListener('sync', event => {
-    console.log('tenemos conexión');
-    console.log(event.tag);
-})
-
-//PUSH: Manejar las push notifications.
-
-self.addEventListener('push', event => {
-    console.log('Notificación recibida.');
-})
